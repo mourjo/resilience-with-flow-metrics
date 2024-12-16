90fec6d5a6af33e57cda75e04a9f1bbc9dfc4697
Mourjo Sen <hello@mourjo.me>
2024-12-16 08:10:20 +0100

    Use deferred result

--- src/main/java/me/mourjo/conduit/api/Controller.java
+++ src/main/java/me/mourjo/conduit/api/Controller.java
@@ -1,14 +1,35 @@
 package me.mourjo.conduit.api;
 
 import java.util.Map;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
 @RestController
 public class Controller {
 
+    final ExecutorService executor = Executors.newFixedThreadPool(5);
+
     @GetMapping("/hello")
-    public Map<String, String> hello(){
-        return Map.of("message", "hello world");
+    public DeferredResult<Map<String, String>> hello(){
+
+        return submit();
+    }
+
+    private DeferredResult<Map<String, String>> submit(){
+        var result = new DeferredResult<Map<String, String>>();
+        executor.submit(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            result.setResult(Map.of("message", "Hello World!"));
+        });
+
+        return result;
     }
 }
