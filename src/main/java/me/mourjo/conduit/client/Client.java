74580dc464a5c4943354beef220f5c45e401a526
Mourjo Sen <hello@mourjo.me>
2025-01-05 08:54:53 +0100

    Add client for NLS server

---
+++ src/main/java/me/mourjo/conduit/client/Client.java
@@ -0,0 +1,27 @@
+package me.mourjo.conduit.client;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestClient;
+
+@EnableScheduling
+@Configuration
+public class Client {
+
+    @Scheduled(fixedRate = 100)
+    public void requests() {
+        RestClient restClient = RestClient.builder()
+            .baseUrl("http://localhost:8080")
+            .build();
+
+        String response = restClient.get()
+            .uri("/hello")
+            .retrieve()
+            .body(String.class);
+
+        System.out.println("got response: " + response);
+
+    }
+
+}
